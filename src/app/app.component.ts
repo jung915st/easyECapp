8c14a452542024ee1699dee31fffe76c51fa1d47
jung915st <jung915@st.tc.edu.tw>
2021-12-31 08:21:23 +0800

    routing setup completed\

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+// Constant
+import { appPath } from './app-path.const';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'easyECapp';
+
+/**
+ * @memberof AppComponent
+ */
+path = appPath;
+
 }
